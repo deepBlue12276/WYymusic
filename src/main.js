0a2f3c51f5e683713b54215c74ec6f8df9c1bc40
lujh7 <lujh7@asiainfo.com>
2021-12-07 10:30:02 +0800

    卸载eslint

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from '@/router/router'
 import store from './store'
+
+import '@/style/common.less'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: function (h) { return h(App) }
+  render: h=>h(App),
 }).$mount('#app')
